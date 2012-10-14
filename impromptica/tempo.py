e9595f8fb7b4f708443e4012b44c68767a8704a4
chris-hudson <chris.hudson@ku.edu>
2012-10-13 21:31:53 -0500

    first attempt at bpm calculation

--- impromptica/tempo.py
+++ impromptica/tempo.py
@@ -7,7 +7,7 @@ Works with audio formatted in 1-D numpy arrays.
 from numpy import copy
 
 
-def map_pass(samples, low_bpm, high_bpm):
+def map_pass(samples, frame_rate, low_bpm, high_bpm):
     """
     Estimates the consistent BPM for an audio sample based on
     the peaks in amplitude and the best fit bpm between the range
@@ -17,15 +17,35 @@ def map_pass(samples, low_bpm, high_bpm):
     #5% of samples with the largest amplitudes
     top_samples = copy(samples)
     top_samples.sort()
-    top_samples = top_samples[-len(top_samples) * 0.05:]
+    top_samples = top_samples[-len(top_samples) * 0.10:]
 
     cut = min(top_samples)
-    print(cut)
 
     filtered_samples = copy(samples)
 
     for pos, sample in enumerate(filtered_samples):
-        if sample < cut:
+        if abs(sample) < cut:
             filtered_samples[pos] = 0
 
-    return filtered_samples
+    best_bpm = 0
+    best_ratio = 0
+
+    #Try to map every bpm in the range, and see which one best fits
+    for bpm in range(low_bpm, high_bpm + 1):
+        sample_rate_step = frame_rate * 60.0 / bpm
+        cur_sample = sample_rate_step
+        hits = 0.0
+        steps = 0.0
+        while cur_sample < len(filtered_samples):
+            steps += 1
+            if filtered_samples[cur_sample] != 0:
+               hits += 1 
+            cur_sample += sample_rate_step
+
+        hit_ratio = hits/steps
+
+        if(hit_ratio > best_ratio):
+            best_bpm = bpm
+            best_ratio = hit_ratio
+
+    return best_bpm
