d1f3425901f4deb929d5556e24ddcaeb09e2c48e
Chris Hudson <chris.hudson@ku.edu>
2012-11-25 20:27:26 -0600

    PEP8 compliance

--- impromptica/utils/note_freq.py
+++ impromptica/utils/note_freq.py
@@ -4,44 +4,52 @@ reports the note frequency (or frequencies) for each onset
 """
 
 from scipy import fft
-from pylab import *
-import numpy
+from pylab import arange, plot, show, subplot, title
+
 
 def frequencies(onsets, samples, Fs=44100):
     """
-    Returns a dict containing lists of frequencies (in hz) corresponding to each onset
+    Returns a dict containing lists of frequencies (in hz) corresponding
+    to each onset.
     Assumes samples is collapsed (mono)
     """
-    #Arbitrary window size - change to something in terms of the sampling frequency?
+    #Arbitrary window size -
+    # change to something in terms of the sampling frequency?
     #Seems to work for now
     NFFT = 1024
 
-    #FFT returns frequency buckets. The first index is 0Hz, and each subsequent one is i * Fs/NFFT Hz
-    freq_dist = Fs/NFFT
+    #FFT returns frequency buckets.
+    #The first index is 0Hz, and each subsequent one is i * Fs/NFFT Hz
+    freq_dist = Fs / NFFT
 
     notes = {}
 
     for notenum, onset in enumerate(onsets):
         #Get the fft
-        transform = fft(samples[onset: onset + NFFT])/NFFT
+        transform = fft(samples[onset: onset + NFFT]) / NFFT
 
-        #Get the magnitude of each FFT coefficient, since it returns complex values
-        fft_coeffs = [x.real**2 + x.imag**2 for x in transform]
+        #Get the magnitude of each FFT coefficient,
+        #since it returns complex values
+        fft_coeffs = [x.real ** 2 + x.imag ** 2 for x in transform]
 
-        #We can only use the first half of the coeffs (See nyquist frequency). The other half are duplicates
-        fft_coeffs = fft_coeffs[:NFFT/2]
+        #We can only use the first half of the coeffs (See nyquist frequency).
+        #The other half are duplicates
+        fft_coeffs = fft_coeffs[:NFFT / 2]
 
-        #Slight misnomer. Contains a list of frequencies associated with this note onset
+        #Slight misnomer.
+        #Contains a list of frequencies associated with this note onset
         note = []
 
-        #Go through each note and figure out the dominant frequencies (find max, accept within a threshold?)
+        #Go through each note and figure out the dominant frequencies
+        #(find max, accept within a threshold?)
         max_coeff = max(fft_coeffs)
         prom_note = fft_coeffs.index(max_coeff) * freq_dist
-        print "Prominent note freq for note %d is %d" % (notenum, prom_note) 
+        print "Prominent note freq for note %d is %d" % (notenum, prom_note)
 
         note.append(prom_note)
-        threshold = .40 * max_coeff #Arbitrary threshold. Probably should make this less hardcoded
 
+        #Arbitrary threshold. Probably should make this less hardcoded
+        threshold = .40 * max_coeff
 
         for i, val in enumerate(fft_coeffs):
             if val > threshold and val != max_coeff:
@@ -49,26 +57,27 @@ def frequencies(onsets, samples, Fs=44100):
 
         notes[onset] = note
 
-    #spam all the notes! (testing)
-    #for notenum, onset in enumerate(onsets):
-    #     graph_title = "Note %d" % notenum
-    #     plotNoteFrequencies(onset, samples, 44100, NFFT, graph_title)
-
     for notenum, note in enumerate(sorted(notes)):
         print "Onset %d notes (Hz): " % notenum, notes[note]
 
     return notes
 
+
 def plotNoteFrequencies(onset, samples, Fs, NFFT, graph_title=""):
-    freq_dist = Fs/NFFT
+    """
+    Visualize the frequency spectogram. Used for testing.
+    """
+    freq_dist = Fs / NFFT
 
     note_samples = samples[onset: onset + NFFT]
-    x = fft(note_samples)/NFFT
-    x = x[:len(x)/2] #Can only use half the fft values (past that, values are duplicated)
-    t = arange(0.0, Fs/2 - freq_dist, freq_dist)
+    x = fft(note_samples) / NFFT
+
+    #Can only use half the fft values. See above comment
+    x = x[:len(x) / 2]
+    t = arange(0.0, Fs / 2 - freq_dist, freq_dist)
 
     title(graph_title)
-    subplt = subplot(111)
+    subplot(111)
     plot(t, abs(x))
-    
+
     show()
