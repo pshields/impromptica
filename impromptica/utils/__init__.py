96989c5e6c524cb856d28624585a1aa509630fdf
Patrick Shields <pshields@gmail.com>
2012-11-28 18:40:16 -0600

    Fix and add unit tests for note frequency functions

--- impromptica/utils/__init__.py
+++ impromptica/utils/__init__.py
@@ -1,3 +1,7 @@
 from generation import generate_note
 from probabilities import build_distance_profile
 from probabilities import build_key_profile
+from note_freq import equal_temperament_note
+from note_freq import frequencies
+from note_freq import frequency_to_note
+from note_freq import note_to_frequency
